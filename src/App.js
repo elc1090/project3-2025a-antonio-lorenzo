b536eb6c61d732409a553e032b478466a843265d
Antônio Amadeu Dall'Agnol Rohr <aarohr@inf.ufsm.br>
2025-05-26 22:55:12 -0300

    fix: diretorios

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/livros" element={<LivroList />} />
         <Route path="/usuarios" element={<UsuarioList />} />
